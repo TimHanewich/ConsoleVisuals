afd36c2265c33fae2ec505f9cafd5a57410bc5ed
Tim Hanewich <tahanewich@live.com>
2022-01-21 17:46:39 -0500

    Added ConsoleVisualsToolkit

---
+++ src/ConsoleVisualsToolkit.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace ConsoleVisuals
+{
+    public class ConsoleVisuals
+    {
+        //Change only the foreground
+        public static void WriteLine(string msg, ConsoleColor foreground)
+        {
+            ConsoleColor oc = Console.ForegroundColor;
+            Console.ForegroundColor = foreground;
+            Console.WriteLine(msg);
+            Console.ForegroundColor = oc;
+        }
+
+        public static void WriteLine(string msg, ConsoleColor foreground, ConsoleColor background)
+        {
+            ConsoleColor orgF = Console.ForegroundColor;
+            ConsoleColor orgB = Console.BackgroundColor;
+
+            Console.ForegroundColor = foreground;
+            Console.BackgroundColor = background;
+
+            Console.WriteLine(msg);
+
+            Console.ForegroundColor = orgF;
+            Console.BackgroundColor = orgB;
+        }
+    }
+}
