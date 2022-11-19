38535297afe2cdeccfb328e23a3cf45d042ac536
Tim Hanewich <tahanewich@live.com>
2022-11-19 08:58:50 -0600

    Updated to NET 6.0

--- testing/Program.cs
+++ testing/Program.cs
@@ -7,11 +7,7 @@ namespace testing
     {
         static void Main(string[] args)
         {
-            ConsoleVisualsToolkit.WriteLine("Hello, world!");
-            ConsoleVisualsToolkit.WriteLine("Hello, world", ConsoleColor.Red);
-            ConsoleVisualsToolkit.WriteLine("Hiii", ConsoleColor.Blue, ConsoleColor.Red);
-            Console.WriteLine("Testing.");
-            ConsoleVisualsToolkit.WriteLine("Testing 123");
+            
         }
     }
 }
