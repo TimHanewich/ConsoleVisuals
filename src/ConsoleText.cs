8596acf8ab98c1d4c48e23f0041cf432c6c9d080
Tim Hanewich <tahanewich@live.com>
2022-11-19 09:05:21 -0600

    Added constructors

--- src/ConsoleText.cs
+++ src/ConsoleText.cs
@@ -13,5 +13,23 @@ namespace ConsoleVisuals
             Foreground = null;
             Background = null;
         }
+
+        public ConsoleText(string text)
+        {
+            Text = text;
+        }
+
+        public ConsoleText(string text, ConsoleColor foreground)
+        {
+            Text = text;
+            Foreground = foreground;
+        }
+
+        public ConsoleText(string text, ConsoleColor foreground, ConsoleColor background)
+        {
+            Text = text;
+            Foreground = foreground;
+            Background = background;
+        }
     }
 }
